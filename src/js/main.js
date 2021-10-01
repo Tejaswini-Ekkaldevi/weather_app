26aabe3c9c54c0b497b83862900830e0c4f19c4e
jivan_supe <jivan.supe@indexnine.com>
2021-10-01 18:30:35 +0530

    updated code

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ const name1 = document.querySelector('.name');
 const temp = document.querySelector('.temp');
 const hum = document.querySelector('.hum');
 const desc = document.querySelector('.desc');
-async function getWeather(location) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=matrics&appid=705f0ff804b088684733f84c8cfd1be6`, { mode: 'cors' });
+let temperatureUnit = "metric"; // metric = celsius   and imperial = fahrenheit
+let currentCity = "Solapur";
+async function getWeather() {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=${temperatureUnit}&appid=705f0ff804b088684733f84c8cfd1be6`, { mode: 'cors' });
   // console.log(response);
   const data = await response.json();
   console.log(data);
@@ -13,19 +15,20 @@ async function getWeather(location) {
   const humval = data.main.humidity;
   const descval = data.weather[0].description;
   name1.innerHTML = nameval;
-  temp.innerHTML = `Temperature : ${tempval} &deg;F`;
+  let tempUnitStr = temperatureUnit == "metric" ? "C" : "F";
+  temp.innerHTML = `Temperature : ${tempval} &deg;${tempUnitStr}`;
   hum.innerHTML = `Humidity :${humval}`;
   desc.innerHTML = `Description : ${descval}`;
 }
 
-getWeather('solapur');
+//imperial
+getWeather();
 
 // eslint-disable-next-line no-unused-vars
 function searchCity() {
   const search = document.getElementById('search');
-
-  const newCity = search.value;
-  getWeather(newCity);
+  currentCity = search.value;
+  getWeather();
   search.value = '';
 }
 const temperature = 0;
@@ -35,17 +38,18 @@ let tempType = 'F';
 const switchTemp = (type) => {
   tempType = type;
 
-  const F = document.getElementById('F');
-  const C = document.getElementById('C');
+  const fahrenheit = document.getElementById('fahrenheit');
+  const celsius = document.getElementById('celsius');
 
-  if (tempType === 'F') {
-    temp.innerHTML = `${temperature * (9 / 5) + 32} &deg;F`;
-    F.classList.remove('disabled');
-    C.classList.add('disabled');
-  } else if (tempType === 'C') {
+  if (tempType === 'fahrenheit') {
+    temperatureUnit = 'imperial';
+    fahrenheit.classList.remove('disabled');
+    celsius.classList.add('disabled');
+  } else if (tempType === 'celsius') {
     // eslint-disable-next-line no-mixed-operators
-    temp.innerHTML = `${(temperature - 32) * 5 / 9} &deg;C`;
-    C.classList.remove('disabled');
-    F.classList.add('disabled');
+    temperatureUnit = 'metric';
+    celsius.classList.remove('disabled');
+    fahrenheit.classList.add('disabled');
   }
+  getWeather();
 };
